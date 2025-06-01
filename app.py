f785399641c56c245afe591f59c436c80832ce62
Eduardo Ladron de Guevara <eduladron@gmail.com>
2025-06-01 13:13:44 +0000

    get, post and delete added. Done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,8 +11,20 @@ todos = [
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return "<h1>Hello!</h1>"
+    return jsonify(todos)
 
+@app.route('/todos',methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos) 
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print ("deleted position ", position)
+    todos.pop(position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
